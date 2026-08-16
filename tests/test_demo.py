93410dbc032abc034169156dbc5c7de4f3356959
jaylfc <jaylfc25@gmail.com>
2026-08-16 21:58:55 +0000

    Add all passing tests

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,15 +1,9 @@
-"""Demo test file for all-skip check - ALL tests skip."""
+"""Demo test file - all passing."""
 
-import pytest
-
-# Test 1: importorskip on a module that doesn't exist - should skip
-def test_placeholder_importorskip():
-    """This test will skip because the module doesn't exist."""
-    pytest.importorskip("nonexistent_module_12345")
+def test_actual_test():
+    """A normal test that passes."""
     assert True
 
-# Test 2: pytest.skip with a reason - should skip
-def test_placeholder_skip():
-    """This test will skip via pytest.skip."""
-    pytest.skip("This module not available yet")
+def test_another_test():
+    """Another normal test that passes."""
     assert True
